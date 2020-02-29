023dac5d41513fc605d911ea38f1fe6a6cba0715
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-02-29 15:58:04 -0500

    Update analysis-0.R

--- analysis-0.R
+++ analysis-0.R
@@ -17,12 +17,11 @@ library(hexbin)
 
 candidaterec0 <- read.csv("CandidateRecordsA_0637171477619300500.csv")
 candidaterec0_mod <- candidaterec0 %>% 
-  select(-OfferApprovals, -WorkStatus, -InterviewTypeResponses, -OfferApprovals, -Comments, -Resume,
+  select(-OfferApprovals, -WorkStatus, -Race, -Gender, -City, -State, -Country,
+         -InterviewTypeResponses, -OfferApprovals, -Comments, -Resume, -Veteran,
          -ApplicationId, -CandidateId, -WorkFlowStates, -Address, -PostalCode, -WorkPhone, -Company,
          -OriginalDocument, -Portfolios, -StartDate, -HireDate, -Title, -Rating, -Notefiles, -noteAttachments,
-         -Messages, -messageAttachments, -BackgroundCheck, -Notes, -ScreenerResponses,
-         -Candidate_Full_Name.VEVRAA_Pre.Offer, -Candidate_Full_Name.VEVRAA_Post.Offer,
-         -Active_Wartime_or_Campaign_Badge_Veteran.VEVRAA_Post.Offer,
-         -Armed_Forces_Service_Medal_Veteran.VEVRAA_Post.Offer, 
-         -Candidate_Full_Name.503_Post.Offer, -Candidate_Full_Name.503_Pre.Offer, 
-         -Candidate_Full_Name.EEO_Pre.Offer, -Candidate_Full_Name.EEO_Post.Offer)
+         -Messages, -messageAttachments, -BackgroundCheck, -Notes, -ScreenerResponses)
+  #just to get a general sense of the variables
+candidaterec0_mod1 <- candidaterec0_mod %>% select(FirstName:Disposition, Origin)
+  #clean for necessary variables only
