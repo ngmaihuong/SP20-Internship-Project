6036446ac2493c0f7c58f8f20aa9dc4d6e833d7f
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-02-29 21:40:19 -0500

    Update analysis-0.R

--- analysis-0.R
+++ analysis-0.R
@@ -25,3 +25,27 @@ candidaterec0_mod <- candidaterec0 %>%
   #just to get a general sense of the variables
 candidaterec0_mod1 <- candidaterec0_mod %>% select(FirstName:Disposition, Origin)
   #clean for necessary variables only
+
+candidaterec1 <- read.csv("CandidateRecordsA_1637171515153834517.csv")
+candidaterec2 <- read.csv("CandidateRecordsA_2637171552633563593.csv")
+candidaterec3 <- read.csv("CandidateRecordsA_3637171593902632419.csv")
+candidaterec4 <- read.csv("CandidateRecordsA_4637171658861484928.csv")
+candidaterec5 <- read.csv("CandidateRecordsA_5637171684326650167.csv")
+
+#CODE TESTING ----
+
+#vec1 <- c("a","b","c","d")
+#vec2 <- c(1,2,3,4)
+#vec3 <- c("A","B","C","D")
+#vec4 <- c(T,T,F,T)
+#df1 <- data.frame(vec1,vec2,vec3,vec4)
+
+#vec1 <- c("mot","hai","ba","bon")
+#vec2 <- c("one","two","three","four")
+#vec3 <- c(F,F,F,T)
+#vec4 <- c(9,8,7,6)
+#df2 <- data.frame(vec1,vec2,vec3,vec4)
+
+#df3 <- rbind(df1,df2)"""
+
+#IS THERE A WAY TO MERGE DATA FRAMES WHILE ONLY KEEP THE COLUMNS AS IN candidaterec0_mod1?
